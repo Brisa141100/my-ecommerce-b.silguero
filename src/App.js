82f598a4f1dc4739fa9c1761bf7b89b9a48ae924
brisa silguero <brisa.silguero23@gmail.com>
2023-03-20 22:56:56 -0300

    1er commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// COMPONENTS
+import Header from "./components/Header/Header";
+import NavBar from "./components/NavBar/navbar";
+import CardUser from "./components/CardUser/CardUser";
+import Button from "./components/Button/Button";
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <Header
+          title="Soy el titulo a partir de una prop"
+          subTitle="Soy un subtitulo a partir de una prop"
+        />
+        <div className="UserSection">
+          <CardUser
+            name="Daniel Di Salvo"
+            date="Se unio en Enero 2021"
+            description="Front-end Developer y profesor del Curso de React.js en CoderHouse"
+            img="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+            backgroundColor="yellow"
+          />
+          <CardUser
+            name="Raul Gutierrez"
+            date="Se unió en Abril 2021"
+            description="Scrum Master y UX Designer en Mercado Libre"
+            img="https://react.semantic-ui.com/images/avatar/large/elliot.jpg"
+          />
+          <CardUser
+            name="Francesca Bertotti"
+            date="Se unió en Julio 2021"
+            description="DevOps y profesora del curso de React.js en CoderHouse"
+            img="https://react.semantic-ui.com/images/avatar/large/daniel.jpg"
+            backgroundColor="green"
+          />
+          <CardUser img="https://react.semantic-ui.com/images/avatar/large/daniel.jpg">
+            <AccountCircleIcon />
+          </CardUser>
+        </div>
+        <Button>Press Me!</Button>
+        <Button>Press Me!</Button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./NavBar.css";
+import Shop from "../shop/shop";
+
+const NavBar = () => {
+    return (
+        <nav className="Navigation">
+            <ul className="List-ul">
+                <li>
+                    <h1 style={{color: "white"}}>SO TRENDD</h1>
+                </li>
+                <li >
+                    <a href="www.google.com">Home</a>
+                </li>
+                <li >
+                    <a href="www.google.com">Contact</a>
+                </li>
+                <li >
+                    <a href="www.google.com">About</a>
+                </li>
+              <li>
+                <Shop />
+              </li>
+            </ul>
+        </nav>
+    )
+}; 
+export default NavBar;
