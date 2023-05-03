f795080fc1c1070df49ec4fcfea4af2ec3609a04
Brisa141100 <brisa.silguero23@gmail.com>
2023-05-03 19:57:28 -0300

    where

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -3,18 +3,19 @@ import "./CardList.css";
 import CardProduct from "../CardProduct/CardProduct";
 import Spinner from "./../Spinner/Spinner"
 import { Link } from "react-router-dom";
-
+import { useParams } from "react-router-dom";
 //FIREBASE
 import { db } from "./../../Firebase/Firebaseconfig"
-import { collection, query, getDocs } from "firebase/firestore"
+import { collection, query, getDocs, where } from "firebase/firestore"
 
 
 const ProductsList = () => {
  const [Product, setProductos] = useState([]);
   const [isLoading, setIsLoading] = useState ([true])
+  let { categoria} = useParams();
 
   useEffect(() => {
-    const getProducts= async () => {
+    const getProducts = async () => {
       const q = query(collection(db, "Products"));
       const docs = [];
       const querySnapshot = await getDocs(q);
@@ -23,12 +24,23 @@ const ProductsList = () => {
       })
       setProductos(docs);
     }
-    getProducts();
+     const getProductsByCategory = async () => {
+      const q = query(collection(db, "Products"), where("categoria", "==", categoria));
+      const docs = [];
+     const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      docs.push({ ...doc.data() });
+       })
+      setProductos(docs);
+    }
+
+     if (categoria) {
+      getProductsByCategory()
+     } else {
+      getProducts();
+     }
     setTimeout(() => { setIsLoading(false); }, 1000);
-  }, []);
-
- 
-
+  }, [categoria]);
   return (
   <>
 { isLoading ? (
